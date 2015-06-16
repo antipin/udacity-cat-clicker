1f6da050781a6ca721f0cf299972d782fb29e014
Alexander Antipin <alex@antipin.com>
2015-06-16 10:09:01 +0300

    Added webpack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,12 @@ module.exports = {
                 'css-loader',
                 'autoprefixer-loader?browsers=last 2 versions'
             ].join('!')
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
         }]
     },
     output: {
